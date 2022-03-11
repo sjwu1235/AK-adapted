7310a1afe30536b4ff5f21d9861f43cf40da1c97
SJ Wu <wxxsij001@myuct.ac.za>
2022-03-12 01:16:01 +0200

    feat: added SciHub scraper script scihub.py and accompanying Scihub_inputs.json pluc FileCheck.py

--- File_Check.py
+++ File_Check.py
@@ -3,46 +3,25 @@ from pathlib import Path
 import time
 import pandas as pd
 import os.path
-
-with open(r'inputs.json', 'r') as input_file:
-    input_deets = json.load(input_file)
-
-directory = input_deets['directory']
-datadump_loc = input_deets['datadump']
-datadump=pd.read_excel(datadump_loc)
-start_loc = input_deets['pivots']
-start_year=input_deets['start_year']
-end_year=input_deets['end_year']
-sleep_time=input_deets['sleep_time']
-
-temp = pd.read_excel(directory+"\\Master lists\\"+input_deets['journal_name']+"_master.xlsx")
-temp2 = pd.read_excel(directory+"\\pivots\\AER2_pivots.xlsx")
-'''
-downloaded=0
-for ind in temp.index:
-    pdf_file_name=temp['stable_url'][ind][29:]+".pdf"
-    #print(pdf_file_name+" "+str(os.path.isfile(directory+pdf_file_name)))
-    if(os.path.isfile(directory+"\\"+pdf_file_name)):
-        downloaded+=1
-print(downloaded)
-print(temp.shape)
-
-'''
-
 import shutil
 import os
-src_folder = r"C:\Users\sjwu1\Journal_Data"
-dst_folder = r"C:\Users\sjwu1\Master_\AER_data"
+
+directory = "..\\Path\\to\\journal\\data"    
+temp = pd.read_excel("..\\path\\to\\masterlist_name.xlsx")
+temp2 = pd.read_excel("..\\path\\to\\name_pivots.xlsx")
+
+#src_folder = r"C:\Users\sjwu1\Journal_Data"
+#dst_folder = r"C:\Users\sjwu1\Master_\AER_data"
 
 total=0
-fulllist=0
+fulllist_s_1940=0
 for ind in temp2.index:
     temp3=temp[temp['issue_url']==temp2['issue_url'][ind]]
     downloaded=0
     for ind2 in temp3.index:
-        pdf_file_name=temp['stable_url'][ind2][29:]+".pdf"
+        pdf_file_name=temp['stable_url'][ind2].split('/')[-1]+".pdf"
         if (temp2['year'][ind]>=1940):
-            fulllist+=1
+            fulllist_s_1940+=1
         #print(pdf_file_name+" "+str(os.path.isfile(directory+pdf_file_name)))
         if(os.path.isfile(directory+"\\"+pdf_file_name)):
             downloaded+=1
@@ -53,4 +32,4 @@ for ind in temp2.index:
     print(str(temp2['year'][ind])+" "+str(temp2['no_docs'][ind])+" "+str(temp2['issue_url'][ind])+" "+str(downloaded))
 
 print(total)
-print(fulllist)
+print(fulllist_s_1940)
--- scihub.py
+++ scihub.py
@@ -1,8 +1,6 @@
 import json
-from pathlib import Path
 import time
 import pandas as pd
-import random
 from selenium import webdriver
 from selenium.webdriver.remote.webelement import WebElement
 from webdriver_manager.chrome import ChromeDriverManager
@@ -10,22 +8,28 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.common.by import By
 import os
-import shutil
-import urllib
 import wget
 
 
 
-with open(r'inputs.json', 'r') as input_file:
+with open(r'Scihub_inputs.json', 'r') as input_file:
     input_deets = json.load(input_file)
 
 directory = input_deets['directory']    
-temp = pd.read_excel(directory+"\\Master lists\\QJE_master.xlsx")
-temp2 = pd.read_excel(directory+"\\pivots\\QJE_pivots.xlsx")
-temp1 = pd.read_excel(directory+"\\Scopus\\QJE_SCOPUS.xlsx")
-# Journal page URL
-URL = 'https://sci-hub.se/10.2307/1828153'
-directory = input_deets['directory']
+temp = pd.read_excel(input_deets['master'])
+temp2 = pd.read_excel(input_deets['pivots'])
+
+#temp1 = pd.read_excel(input_deets['scopus'])
+
+processed_list=[]
+issues=temp2[temp2['year']==input_deets['year']]['issue_url']
+for i in issues.index:
+    dois=temp[temp['issue_url']==issues[i]]
+    x=dois['stable_url']
+    for j in x.index:
+        processed_list.append('10.2307'+x[j][x[j].rfind('/'):])
+    print(dois['stable_url'].shape)
+
 
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36'
 chrome_options = webdriver.ChromeOptions()
@@ -41,10 +45,10 @@ chrome_options.add_extension("./extension_busters.crx")
 chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
 chrome_options.add_experimental_option("useAutomationExtension", False)
 chrome_options.add_experimental_option("prefs", {
-    "download.default_directory": "C:\\Users\\sjwu1\\Journal_Data\\Scihub", #Change default directory for downloads
+    "download.default_directory": directory, #Change default directory for downloads
     "download.prompt_for_download": False, #To auto download the file
     "download.directory_upgrade": True,
-    "plugins.always_open_pdf_externally": True, #It will not show PDF directly in chrome
+    #"plugins.always_open_pdf_externally": True, #It will not show PDF directly in chrome
     "credentials_enable_service": False, # gets rid of password saver popup
     "profile.password_manager_enabled": False #gets rid of password saver popup
 })
@@ -54,8 +58,9 @@ driver = webdriver.Chrome(ChromeDriverManager().install(), options = chrome_opti
 
 driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
 time.sleep(5)
-
-temp3=temp1[temp1['year']==1941]
+'''
+# This section uses scopus DOIs which may be necessary for newer issues
+temp3=temp1[temp1['year']==1942]
 print(temp3)
 for i in temp3.index:
     if '10.2307'in temp3['DOI'][i]:
@@ -70,7 +75,21 @@ for i in temp3.index:
             print(DOI)
         except:
             print('not loading, document not likely on SciHub')
-            time.sleep(5)
+            time.sleep(10)
     else:
         print("DOI not in sync with JSTOR: "+temp3['DOI'][i])
+'''
 
+for i in processed_list:
+    if(i[8:]+'.pdf' in os.listdir(directory)):
+        continue
+    driver.get("https://sci-hub.se/"+i)
+    time.sleep(15)
+    try:
+        print(driver.find_element_by_xpath(r".//div[@id='buttons']//button").get_attribute('onclick')[15:-1])
+        wget.download('https:'+driver.find_element_by_xpath(r".//div[@id='buttons']//button").get_attribute('onclick')[15:-1],directory+'\\'+i[8:]+'.pdf')
+        time.sleep(10)
+        print(i)
+    except:
+        print('not loading, document not likely on SciHub')
+        time.sleep(15)
